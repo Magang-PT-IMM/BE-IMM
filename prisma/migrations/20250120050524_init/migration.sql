4931ecc21e2599eca27c544cf036997704f5206c
KipasTerbang <alimsyah071002@gmail.com>
2025-01-22 09:53:55 +0700

    remove migrations and seed

--- prisma/migrations/20250120050524_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "progressTickets" ADD COLUMN     "deletedAt" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "ticketUsers" ADD COLUMN     "deletedAt" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "tickets" ALTER COLUMN "status" DROP NOT NULL;
